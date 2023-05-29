cf475685cf98064b04d4fd697b295e8931e787df
Eze Onyebuchi <eze4acme@gmail.com>
2023-05-29 15:01:34 +0100

    Changes on the UI
    made changes on the UI and functionality.

--- stopwatch.js
+++ stopwatch.js
@@ -1,61 +1,37 @@
-const timeDisplayEl = document.querySelector('.time-display');
-const startEl = document.getElementById('start');
-const stopEl = document.getElementById('pause');
-const resetEl = document.getElementById('reset');
+const btnEl = document.querySelectorAll('button')
+const timeDisplay = document.querySelector('.time-display')
 
-
-let startTime = 0;
+let startTime;
 let elapsedTime = 0;
-let currentTime = 0;
-let paused = true;
-let intervalId;
-let hrs = 0;
-let mins = 0;
-let secs = 0;
-startEl.addEventListener('click', () =>{
-    if(paused){
-        paused = false;
-        startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updateTime, 1000);
-    }
-})
-stopEl.addEventListener('click', () =>{
-    if(!paused){
-        paused = true;
-        elapsedTime = Date.now() - startTime;
-        clearInterval(intervalId)
-    }
+let timerInterval;
 
-})
-resetEl.addEventListener('click', () =>{
-    paused = true;
-    clearInterval(intervalId);
-    startTime = 0;
-    elapsedTime = 0;
-    currentTime = 0;
-    paused = true;
-    intervalId;
-    hrs = 0;
-    mins = 0;
-    secs = 0;
-    timeDisplayEl.innerHTML = `00:00:00`
-
-})
-
-function updateTime() {
-    elapsedTime = Date.now() - startTime;
-
-    secs = Math.floor((elapsedTime/1000) % 60);
-    mins = Math.floor((elapsedTime / (1000*60)) % 60);
-    hrs = Math.floor ((elapsedTime / (1000 * 60 * 60)) % 60);
-    if(secs < 10){
-        secs ='0' + secs;
-    }
-    if(mins < 10){
-        mins ='0' + mins;
-    }
-    if(hrs < 10){
-    hrs ='0' + hrs;
-    }
-   timeDisplayEl.innerHTML = `${hrs}:${mins}:${secs}`
+function formatDisplay(time){
+    const date = new Date(time)
+    const hour = date.getUTCHours().toString().padStart(2, "0")
+    const minutes = date.getUTCMinutes().toString().padStart(2, "0")
+    const seconds = date.getUTCSeconds().toString().padStart(2, "0")
+    const milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0")
+    timeDisplay.innerHTML = `${hour}:${minutes}:${seconds}:${milliseconds}`
 }
+
+btnEl.forEach((btn) =>{
+    btn.addEventListener('click', function(event) {
+        const btnClass = event.currentTarget.classList
+        if (btnClass.contains('start-btn')) {
+          startTime = Date.now() - elapsedTime
+            timerInterval = setInterval(() => {
+                elapsedTime = Date.now() - startTime
+                formatDisplay(elapsedTime)
+            }, 10);
+        }
+        if (btnClass.contains('stop-btn')) {
+            clearInterval(timerInterval)
+        }
+        if (btnClass.contains('reset-btn')) {
+            clearInterval(timerInterval)
+            elapsedTime = 0;
+            formatDisplay(elapsedTime)
+        }
+    })
+})
+
